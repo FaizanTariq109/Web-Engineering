fc52359ac4f7fd56c17d1bca6df1aca88ee03efa
FaizanTariq <faizan3san@gmail.com>
2025-02-18 13:15:30 +0500

    18th Feb Lecture

---
+++ Feb18/script.js
@@ -0,0 +1,36 @@
+// console.log("Before");
+// fetchUser(123, function (user) {
+//   console.log(user);
+// });
+
+// function fetchUser(userid, callback) {
+//   setTimeout(function () {
+//     const fetchedUser = {
+//       id: userid,
+//       name: "Hashim",
+//       email: "Hashim@email.com",
+//       age: 27,
+//     };
+//     callback(fetchedUser);
+//   }, 2000);
+// }
+// console.log("End");
+
+// console.log("Before");
+// function sendEmail(userEmail, callback) {
+//   setTimeout(function () {
+//     const response = {
+//       status: true,
+//     };
+//     callback(response);
+//   }, 3000);
+// }
+// sendEmail("Hashim@gmail.com", function (response) {
+//   console.log(response);
+// });
+// console.log("End");
+
+// const getAPI = fetch("https://api.github.com/users/hashimaliii");
+// getAPI.then((res) => console.log(res));
+// getAPI.catch((err) => console.log(err.message));
+// console.log(getAPI);
